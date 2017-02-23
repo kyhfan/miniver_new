22885dd798f6e8723192dd60c5ae19be328a009b
kyh <kyhfan@gmail.com>
2017-02-23 12:28:29 +0900

    폴더 경로 수정

---
+++ include/dir.php
@@ -0,0 +1,43 @@
+<?
+	// 홈페이지 메인 폴더 경로
+	$_mnv_base_dir					= $_SERVER['DOCUMENT_ROOT']."/";
+
+	// 홈페이지 루트폴더
+	$_mnv_include_dir_name			= "include";
+	$_mnv_admin_dir_name			= "admin";
+	$_mnv_css_dir_name				= "css";
+	$_mnv_js_dir_name				= "js";
+	$_mnv_lib_dir_name				= "lib";
+	$_mnv_images_dir_name			= "images";
+	$_mnv_m_dir_name				= "m";
+
+	// 홈페이지 루트폴더 경로
+	$_mnv_include_dir				= $_mnv_base_dir . $_mnv_include_dir_name . "/";
+	$_mnv_admin_dir					= $_mnv_base_dir . $_mnv_admin_dir_name . "/";
+	$_mnv_js_dir					= $_mnv_base_dir . $_mnv_js_dir_name . "/";
+	$_mnv_css_dir					= $_mnv_base_dir . $_mnv_css_dir_name . "/";
+	$_mnv_lib_dir					= $_mnv_base_dir . $_mnv_lib_dir_name . "/";
+	$_mnv_images_dir				= $_mnv_base_dir . $_mnv_images_dir_name . "/";
+	$_mnv_m_dir						= $_mnv_base_dir . $_mnv_m_dir_name . "/";
+
+	// 홈페이지 m폴더 경로
+	$_mnv_m_css_dir					= $_mnv_base_dir . $_mnv_m_dir_name . "/" . $_mnv_css_dir_name . "/";
+	$_mnv_m_images_dir				= $_mnv_base_dir . $_mnv_m_dir_name . "/" . $_mnv_images_dir_name . "/";
+
+	// 홈페이지 메인 URL 경로
+	$_mnv_base_url					= "http://".$_SERVER['SERVER_NAME']."/";
+
+	// 홈페이지 메인 URL 경로
+	$_mnv_include_url				= $_mnv_base_url . $_mnv_include_dir_name . "/";
+	$_mnv_admin_url					= $_mnv_base_url . $_mnv_admin_dir_name . "/";
+	$_mnv_js_url					= $_mnv_base_url . $_mnv_js_dir_name . "/";
+	$_mnv_css_url					= $_mnv_base_url . $_mnv_css_dir_name . "/";
+	$_mnv_lib_url					= $_mnv_base_url . $_mnv_lib_dir_name . "/";
+	$_mnv_images_url				= $_mnv_base_url . $_mnv_images_dir_name . "/";
+	$_mnv_m_url						= $_mnv_base_url . $_mnv_m_dir_name . "/";
+
+	// 홈페이지 m URL 경로
+	$_mnv_m_css_url					= $_mnv_base_url . $_mnv_m_dir_name . "/" . $_mnv_css_dir_name . "/";
+	$_mnv_m_images_url			= $_mnv_base_url . $_mnv_m_dir_name . "/" . $_mnv_images_dir_name . "/";
+
+?>
