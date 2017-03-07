6162f43298be9b57a63b81397254f9c42dcb806b
kyh <kyhfan@gmail.com>
2017-03-07 17:41:12 +0900

    페이지 전환 효과 테스트

--- about.php
+++ about.php
@@ -11,6 +11,7 @@
 	<link href='https://api.mapbox.com/mapbox.js/v3.0.1/mapbox.css' rel='stylesheet' />	
 	<script src="./js/jquery-1.11.2.min.js"></script>
 	<script src="./js/modernizr-custom.js"></script>
+	<script src="./js/main.js"></script>
 	<script src="./lib/bxslider/jquery.bxslider.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.1/TweenMax.min.js"></script>
 	<script src='https://api.mapbox.com/mapbox.js/v3.0.1/mapbox.js'></script>
@@ -31,55 +32,14 @@
 </head>
 <body>
 	<div id="miniver">
-		<div id="header">
-			<div class="header-logo">
-				<a href="about.php">
-					<img src="./images/header_logo.png" alt="MINIVERTISING">
-				</a>
-			</div>
-			<div class="contact-us">
-				<a href="contactUs">
-					<img src="./images/contact_us.png" alt="CONTACT US">
-					<!-- <i class="fa fa-angle-right first" aria-hidden="true"></i> -->
-					<!-- <i class="fa fa-angle-right second" aria-hidden="true"></i> -->
-				</a>
-			</div>
-			<div class="sns-elems">
-				<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 15 15" class="fb-svg">
-					<path d="M4.7,0v1.7H3.6c-0.3,0-0.5,0.2-0.5,0.5v1h1.6L4.5,5H3.1v5H1V5.1H0V3.2h0.9V2c0-1.1,0.7-2,2-2H4.7z" fill="#ffffff">
-				</svg>
-				<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="26" height="20" viewBox="0 0 16 16" class="blog-svg">
-					<path d="M5.1,3.8c0.8-0.4,1.2-1,1.2-1.8c0-0.2,0-0.5-0.1-0.7C6.1,1.1,6,0.9,5.8,0.8C5.7,0.6,5.5,0.5,5.3,0.3
-						C5.1,0.2,4.8,0.1,4.6,0.1S4,0,3.6,0H1C0.6,0,0.4,0.1,0.2,0.2C0.1,0.4,0,0.6,0,1v6c0,0.3,0.1,0.6,0.2,0.8C0.4,7.9,0.6,8,1,8h2.4
-						c0.4,0,0.8,0,1.1-0.1c0.3,0,0.6-0.1,0.8-0.2c0.4-0.2,0.7-0.4,0.9-0.8c0.2-0.4,0.3-0.8,0.3-1.2C6.6,4.8,6.1,4.1,5.1,3.8z M1.6,1.2
-						h1.3c0.6,0,1,0.1,1.3,0.2c0.3,0.1,0.4,0.4,0.4,0.8c0,0.2,0,0.4-0.1,0.5C4.4,2.9,4.3,3.1,4,3.2C3.8,3.2,3.5,3.3,3.1,3.3H1.6V1.2z
-						M3.4,6.8H1.6V4.4h1.7c0.6,0,1,0.1,1.3,0.3C4.9,4.9,5,5.2,5,5.6C5,6.4,4.5,6.8,3.4,6.8z" fill="#ffffff">
-				</svg>
-			</div>
-		</div>
 <?
+	// 사이드 헤더 메뉴
+	include_once "header.php";
+	// CONTACT US 팝업
 	include_once "contact.php";
+	// 상단 햄버그 메뉴
+	include_once "navi.php";
 ?>		
-		<div id="nav">
-			<div class="wrap-btn">
-				<div class="hamburger">
-					<div class="nav-line"></div>
-					<div class="nav-line"></div>
-					<div class="nav-line"></div>
-				</div>
-				<div class="cross">
-					<div class="nav-line"></div>
-					<div class="nav-line"></div>
-				</div>
-			</div>
-			<div class="wrap-menu">
-				<ul class="clearfix">
-					<li class="active"><a href="javascript:void(0)"><img src="./images/navi_01.png" alt="about"></a></li>
-					<li><a href="work_list"><img src="./images/navi_02.png" alt="work"></a></li>
-					<li><a href="javascript:void(0)"><img src="./images/navi_03.png" alt="news"></a></li>
-				</ul>
-			</div>
-		</div>
 		<div class="content-outer">
 			<div class="content-inner main">
 				<div class="slider-background-wrapper">
@@ -143,6 +103,16 @@
 			speed:1000,
 			auto:true,
 			pager: false
+		});
+
+		$("#header").fadeIn(300, function(){
+			$("#nav").fadeIn(300, function(){
+				//$(".content-outer").fadeIn(300);
+				//TweenMax.staggerFrom('.content-outer', .5, {opacity: 1}, 0.1);
+				$(".content-outer").css("visibility","visible");
+				 TweenMax.from( $('.content-outer'), 0.5, {css:{opacity:0}});
+				//TweenMax.staggerFrom("li", .5, {top:100, opacity:0, delay:1, ease:Back.easeOut}, 0.1);
+			});
 		});
 	});
 
---
+++ navi.php
@@ -0,0 +1,20 @@
+		<div id="nav">
+			<div class="wrap-btn">
+				<div class="hamburger">
+					<div class="nav-line"></div>
+					<div class="nav-line"></div>
+					<div class="nav-line"></div>
+				</div>
+				<div class="cross">
+					<div class="nav-line"></div>
+					<div class="nav-line"></div>
+				</div>
+			</div>
+			<div class="wrap-menu">
+				<ul class="clearfix">
+					<li class="active"><a href="javascript:void(0)"><img src="./images/navi_01.png" alt="about"></a></li>
+					<li><a href="javascript:void(0);" onclick="move_page('work.html');"><img src="./images/navi_02.png" alt="work"></a></li>
+					<li><a href="javascript:void(0);" onclick="move_page('news.html');"><img src="./images/navi_03.png" alt="news"></a></li>
+				</ul>
+			</div>
+		</div>
