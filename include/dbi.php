bb9a83ef59165501cf23e401249b6918fa7cfcab
ojoonwoo <jw.o@minivertising.kr>
2017-03-09 19:21:48 +0900

    no message

--- include/dbi.php
+++ include/dbi.php
@@ -7,8 +7,8 @@
 //	$my_db = new mysqli("localhost", "root", "7alslqjxkdlwld@%*)1590", "miniver");
 	//$my_db = new mysqli("localhost", "root", "miniver_2017", "miniver_home");
 	// $my_db = new mysqli("localhost", "root", "1234", "miniver_home");
-	$my_db = new mysqli("localhost", "minivertising", "alslqj~1", "minivertising");
-	// $my_db = new mysqli("localhost", "miniver", "miniver_2016", "miniver_home");
+	// $my_db = new mysqli("localhost", "minivertising", "alslqj~1", "minivertising");
+	$my_db = new mysqli("localhost", "miniver", "miniver_2016", "miniver_home");
 	if (mysqli_connect_error()) {
 		exit('Connect Error (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
 	}
