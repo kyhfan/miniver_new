9dcd930bdc3a8f0dad43d50748eee3dc94594ae6
kyh <kyhfan@gmail.com>
2017-03-09 14:48:22 +0900

    이미지 업로드

--- work_detail.php
+++ work_detail.php
@@ -16,6 +16,10 @@
 	include_once "navi_w.php";
 ?>		
 		<div class="content-outer">
+<?
+    if ($portfolio_info["template_gubun"] == "web")
+    {
+?>            
 			<div class="content-inner workdetail descAndPhone">
 				<div class="device-block">
 					<div class="layout"></div>
@@ -89,6 +93,155 @@
 					</div>
 				</div>
 			</div>
+<?
+    }else if ($portfolio_info["template_gubun"] == "tablet"){
+?>            
+			<div class="content-inner workdetail descAndTablet">
+				<div class="device-block">
+					<div class="layout"></div>
+					<div class="brand"><?=$portfolio_info['project_company_name']?></div>
+					<div class="title">
+						'<?=$portfolio_info['project_name']?>'
+					</div>
+					<div class="mock-up">
+						<div class="img">
+							<img src="<?=str_replace("../../../","",$portfolio_info['main_image'])?>" alt="">
+						</div>
+					</div>
+				</div>
+				<div class="desc-block clearfix">
+					<div class="papers">
+						<div class="paper _01 animate" data-animation-name="paperDrop">
+							<img src="<?=str_replace("../../../","",$portfolio_info['middle_image3'])?>" alt="">
+						</div>
+						<div class="paper _02 animate" data-animation-name="paperDrop">
+							<img src="<?=str_replace("../../../","",$portfolio_info['middle_image2'])?>" alt="">
+						</div>
+						<div class="paper _03 animate" data-animation-name="paperDrop">
+							<img src="<?=str_replace("../../../","",$portfolio_info['middle_image1'])?>" alt="">
+						</div>
+					</div>
+					<div class="desc animate" data-animation-name="fadeInUp">
+						<div class="row category">
+							<span class="guide">Category : </span>
+							<span class="input"><?=$portfolio_info['project_category']?></span>
+						</div>
+						<div class="row client">
+							<span class="guide">Client : </span>
+							<span class="input"><?=$portfolio_info['project_client']?></span>
+						</div>
+						<div class="block issue">
+							<div class="title">
+								<span>brand issue</span>
+							</div>
+							<div class="sub">
+								<p>
+									<?=$portfolio_info['project_brand_issue']?>
+								</p>
+							</div>
+						</div>
+						<div class="block creative">
+							<div class="title">
+								<span>creative</span>
+							</div>
+							<div class="sub">
+								<p>
+									<?=$portfolio_info['project_creative']?>
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="spread-block">
+					<div class="layout">
+						<div class="spread-pages animate" data-animation-name="fadeInUp" data-animation-delay="2">
+							<img src="<?=str_replace("../../../","",$portfolio_info['web_image1'])?>" alt="">
+							<img src="<?=str_replace("../../../","",$portfolio_info['web_image2'])?>" alt="">
+							<img src="<?=str_replace("../../../","",$portfolio_info['web_image3'])?>" alt="">
+						</div>
+					</div>
+					<div class="list-all">
+						<a href="javascript:void(0);" onclick="move_page('work_list.php');">
+							<span>전체목록</span>
+						</a>
+					</div>
+				</div>
+			</div>
+<?
+    }else if ($portfolio_info["template_gubun"] == "video"){
+?>            
+			<div class="content-inner workdetail descAndPhone video">
+				<div class="device-block">
+					<div class="layout"></div>
+					<div class="brand">더페이스샵 CC 쿠션</div>
+					<div class="title">
+						'예뻐지는 가방을 찾습니다'
+					</div>
+					<!-- video  -->
+					<div class="video-frame mock-up">
+						<img src="./images/video_sample.png" alt="">
+					</div>
+					<!-- video  -->
+				</div>
+				<div class="desc-block clearfix">
+					<div class="desc animate" data-animation-name="fadeInUp">
+						<div class="row clearfix">
+							<div class="category">
+								<span class="guide">Category : </span>
+								<span class="input">DIGITAL CAMPAIGN / VIRAL FILM, ON&OFFLINE EVENT</span>
+							</div>
+							<div class="client">
+								<span class="guide">Client : </span>
+								<span class="input">THEFACESHOP</span>
+							</div>
+						</div>
+						<div class="block issue">
+							<div class="title">
+								<span>brand issue</span>
+							</div>
+							<div class="sub">
+								<p>
+									‘더페이스샵 쿠션’ X 마이아더백과 콜라보레이션 런칭
+								</p>
+							</div>
+						</div>
+						<div class="block creative">
+							<div class="title">
+								<span>creative</span>
+							</div>
+							<div class="sub">
+								<p>
+									콜라보레이션한 쿠션의 컨셉을 ‘예뻐지는 가방’으로 제안하고, 가방을 찾는 게임과 캠페인 사이트를 통해 소비자가 지하철 사물함에서 ‘예뻐지는 가방(쿠션)’을 찾는
+									캠페인을 기획 진행 하였습니다 온라인과 오프라인 연계한 캠페인으로 각 지하철 역마다 현장에 수많은 인파가 모이는 광경이 펼쳐 졌습니다 각지역 소비자의
+									요청에 따른  대구 부산 추가 진행이 이어졌습니다
+								</p>
+							</div>
+						</div>
+						<div class="capture-block animate" data-animation-name="fadeInUp" data-animation-delay="1">
+							<img src="./images/capture_sample.png" alt="">
+						</div>
+					</div>
+				</div>
+				<div class="spread-block">
+					<div class="layout">
+						<div class="spread-pages animate" data-animation-name="fadeInUp" data-animation-delay="1">
+							<img src="./images/thefaceshop_spread_01.png" alt="">
+							<img src="./images/thefaceshop_spread_02.png" alt="">
+							<img src="./images/thefaceshop_spread_03.png" alt="">
+							<img src="./images/thefaceshop_spread_04.png" alt="">
+							<img src="./images/thefaceshop_spread_05.png" alt="">
+						</div>
+					</div>
+					<div class="list-all">
+						<a href="#">
+							<span>전체목록</span>
+						</a>
+					</div>
+				</div>
+			</div>
+<?
+    }
+?>            
 		</div>
 	</div>
 <script type="text/javascript">
