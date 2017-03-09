eec045c88175b3b9f43e491a160b6062c574620a
kyh <kyhfan@gmail.com>
2017-03-09 10:48:48 +0900

    DB 연결 정보 변경

--- include/dbi.php
+++ include/dbi.php
@@ -4,8 +4,8 @@
 *	php function 
 *
 */
-//	$my_db = new mysqli("localhost", "root", "7alslqjxkdlwld@%*)1590", "miniver");
-	$my_db = new mysqli("localhost", "root", "miniver_2017", "miniver_home");
+	//$my_db = new mysqli("localhost", "root", "miniver_2017", "miniver_home");
+	$my_db = new mysqli("localhost", "minivertising", "alslqj~1", "minivertising");
 	if (mysqli_connect_error()) {
 		exit('Connect Error (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
 	}
