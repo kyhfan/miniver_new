8e290d3e8b594feaace276a30cacd089e46f4e73
kyh <kyhfan@gmail.com>
2017-03-14 15:45:40 +0900

    코드 수정

---
+++ m/header.php
@@ -0,0 +1,40 @@
+		<div class="header">
+			<div class="header-outer">
+				<div class="header-inner clearfix">
+					<a href="" class="logo">
+						<span class="sr-only">MINIVERTISING</span>
+					</a>
+					<div class="nav">
+						<div class="wrap-btn">
+							<div class="hamburger">
+								<div class="nav-line"></div>
+								<div class="nav-line"></div>
+								<div class="nav-line"></div>
+							</div>
+							<div class="cross">
+								<div class="nav-line"></div>
+								<div class="nav-line"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+        <div class="menu-layer">
+            <div class="inner">
+                <div class="menu-list">
+                    <a href="#" class="about">
+                        <img src="./images/navi_01.png" alt="about">
+                    </a>
+                    <a href="#" class="work current">
+                        <img src="./images/navi_02.png" alt="work">
+                    </a>
+                    <a href="#" class="news">
+                        <img src="./images/navi_03.png" alt="news">
+                    </a>
+                    <a href="#" class="contactUs">
+                        <img src="./images/navi_04.png" alt="contact us">
+                    </a>
+                </div>
+            </div>
+        </div>
